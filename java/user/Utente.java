7b3d6e7e6b3713ff9171418c664ee247210fb1ec
paolo <paolo.isella@mail.polimi.it>
2017-11-28 13:12:27 +0000

    Upload New File

---
+++ java/user/Utente.java
@@ -0,0 +1,16 @@
+package client;
+import java.util.ArrayList;
+import autorizzatore.Token;
+
+public class Utente {
+
+	private String nome;
+	private String chiave;
+	private ArrayList<Token> tokens;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
