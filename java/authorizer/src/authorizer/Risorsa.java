586721e21710a9581904e5457cce155dbe522db6
paolo <paolo.isella@mail.polimi.it>
2017-11-28 14:16:28 +0000

    Upload New File

---
+++ java/authorizer/src/authorizer/Risorsa.java
@@ -0,0 +1,40 @@
+package autorizzatore;
+
+interface Risorsa {
+	int getLivello();
+	boolean verificaToken();
+}
+
+interface FactoryRisorsa {
+	Risorsa creaRisorsa();
+}
+
+class Risorsa1 implements Risorsa {
+	private Risorsa1() {}	
+	public int getLivello() {
+		System.out.print("Risorsa1  getLivello()");
+		return 1;
+	}
+	public boolean verificaToken() {
+		System.out.print("Risorsa1 verificaToken");
+		return true;
+	}
+	public static FactoryRisorsa factory =	new FactoryRisorsa() {
+		public Risorsa creaRisorsa() {return new Risorsa1();}
+	};
+}
+
+class Risorsa2 implements Risorsa {
+	private Risorsa2() {}	
+	public int getLivello() {
+		System.out.print("Risorsa2  getLivello()");
+		return 1;
+	}
+	public boolean verificaToken() {
+		System.out.print("Risorsa2 verificaToken");
+		return true;
+	}
+	public static FactoryRisorsa factory =	new FactoryRisorsa() {
+		public Risorsa creaRisorsa() {return new Risorsa2();}
+	};
+}
