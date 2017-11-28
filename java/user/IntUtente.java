1014900a32e89a1a0b93c0cc99d551332c8cb47c
paolo <paolo.isella@mail.polimi.it>
2017-11-28 13:12:56 +0000

    Upload New File

---
+++ java/user/IntUtente.java
@@ -0,0 +1,10 @@
+package client;
+import autorizzatore.Autorizzazione;
+import java.util.Date;
+
+public interface IntUtente {
+	Autorizzazione creaAutorizzazione(String _nomeUtente, int _livello, Date _scadenza);
+	boolean revocaAutorizzazione(String chiave);
+	String verificaEsistenzaAutorizzazione();
+	String creaToken(String chiave, String idRisorsa);
+}
