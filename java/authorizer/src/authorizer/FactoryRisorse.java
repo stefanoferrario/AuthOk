0007aeeebb4a8562937b1861313871844c29dd4c
paolo <paolo.isella@mail.polimi.it>
2017-12-02 12:07:16 +0000

    Upload New File

---
+++ java/authorizer/src/authorizer/FactoryRisorse.java
@@ -0,0 +1,22 @@
+package autorizzatore;
+
+class FactoryRisorsaPdf implements FactoryRisorsa{
+	public RisorsaPdf creaRisorsa() {
+		return new RisorsaPdf();
+	}
+}
+
+class FactoryRisorsaExcel implements FactoryRisorsa{
+	public RisorsaExcel creaRisorsa() {
+		return new RisorsaExcel();
+	}
+}
+
+public class FactoryRisorse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
