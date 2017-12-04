ce02960cadb030e733ebacef7ab957a986bf71f2
paolo <paolo.isella@mail.polimi.it>
2017-12-04 12:54:01 +0000

    Upload New File

---
+++ java/authorizer/src/authorizer/Gestore Risorse/RisorsaExcel.java
@@ -0,0 +1,23 @@
+package gestorerisorse;
+import java.util.Date;
+
+class RisorsaExcel implements IRisorsa {
+	//campi:
+	private int livello = 0;
+	private String id = null;
+	
+	//metodi:
+	public RisorsaExcel() { System.out.println("RisorsaExcel()"); }	
+	
+	public void setLivello(int _livello) { livello = _livello; }
+	
+	public int getLivello() { return livello; }
+	
+	public String getId() { return id; }
+	
+	@SuppressWarnings("unused")
+	private Date verificaToken() {
+		System.out.println("RisorsaExecel.verificaToken() - viene ritornata semplicemente la data attuale per adesso");
+		return new Date();
+	}
+}
