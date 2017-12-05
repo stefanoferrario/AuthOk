620f50363bc7d4202505e64a660c67b0924a3ce9
TayGit <tayfun.gumus@mail.polimi.it>
2017-12-06 00:04:14 +0100

    Autorizzazione aggiornato

--- java/authorizer/src/authorizer/GestoreAutorizzazioni/GestoreAutorizzazioni.java
+++ java/authorizer/src/authorizer/GestoreAutorizzazioni/GestoreAutorizzazioni.java
@@ -3,9 +3,11 @@ package authorizer.GestoreAutorizzazioni;
 import authorizer.GestoreAutorizzazioni.Autorizzazione;
 import java.util.Date;
 import java.util.HashMap;
+//import java.util.UUID;
 
 class GestoreAutorizzazioni {
     private static GestoreAutorizzazioni instance = null;
+
     private HashMap<String,Autorizzazione> autorizzazioni = null;
 
     //Singleton Design pattern
@@ -19,6 +21,26 @@ class GestoreAutorizzazioni {
 
     String creaAutorizzazione(String nomeUtente,int livello,Date scadenza){
 
+       if(this.verificaEsistenzaAutorizzazione(nomeUtente)){
+            return "";
+       }
+
+
+       Autorizzazione auth = new Autorizzazione(nomeUtente,livello,scadenza);
+       //String uniqueID = UUID.randomUUID().toString();
+       autorizzazioni.put(nomeUtente,auth);
+
        return (nomeUtente+"-"+livello+"-"+scadenza);
     }
+
+    void revocaAutorizzazione(String nomeUtente){
+        autorizzazioni.remove(nomeUtente);
+    }
+
+    boolean verificaEsistenzaAutorizzazione(String nomeUtente){
+
+        return autorizzazioni.containsKey(nomeUtente);
+
+    }
+
 }
