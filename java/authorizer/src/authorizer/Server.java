bb1e9f3dcda50ae6c020588a695fd479d5402b6b
fmartinese <federico.martinese@mail.polimi.it>
2017-12-07 20:09:37 +0100

    Aggiunti metodi nel server

--- java/authorizer/src/authorizer/Server.java
+++ java/authorizer/src/authorizer/Server.java
@@ -4,9 +4,19 @@ import authorizer.GestoreRisorse.GestoreRisorse;
 import authorizer.GestoreToken.GestoreToken;
 import authorizer.GestoreAutorizzazioni.GestoreAutorizzazioni;
 import jsonrpc.*;
+import jsonrpc.IServer;
 import jsonrpc.Error;
+import jsonrpc.Member;
+import jsonrpc.StructuredMember;
+import jsonrpc.Request;
+import jsonrpc.Response;
 import java.security.InvalidParameterException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
 
 public class Server {
     private Server instance = null;
@@ -40,33 +50,33 @@ public class Server {
         }
 
         ArrayList<Response> resps = new ArrayList<>();
+
         for (Request req : reqs) {
-            if (req.isNotify()) {
+            Response resp = null;
+            try {
                 try {
-                    selectMethod(req.getMethod(), req.getParams());
-                } catch (Exception e) {
-                    //le notifiche non restituiscono errori
+                    Member result = selectMethod(Methods.valueOf(req.getMethod()), req.getParams());
+                    resp = new Response(req.getId(), result);
+                } catch (InvalidParameterException e) {
+                    Error error = new Error(Error.Errors.INVALID_PARAMS);
+                    resp = new Response(req.getId(), error);
+                } catch (IllegalArgumentException e) {
+                    //lanciata dal Methods.valueOf() se la stringa non corrisponde a un metodo
+                    Error error = new Error(Error.Errors.METHOD_NOT_FOUND);
+                    resp = new Response(req.getId(), error);
+                } catch (UnsupportedOperationException e) {
+                    //errore
                 }
-            } else {
-                try {
-                    try {
-                        Member result = selectMethod(req.getMethod(), req.getParams());
-                        resps.add(new Response(req.getId(), result));
-                    } catch (NoSuchMethodException e) {
-                        Error error = new Error(Error.Errors.METHOD_NOT_FOUND);
-                        resps.add(new Response(req.getId(), error));
-                    } catch (InvalidParameterException e) {
-                        Error error = new Error(Error.Errors.INVALID_PARAMS);
-                        resps.add(new Response(req.getId(), error));
-                    } catch (UnsupportedOperationException e) {
-                        //errore
-                    }
-                } catch (JSONRPCException e) {
-                    System.out.println(e.getMessage());
-                    resps.add(null);
+            } catch (JSONRPCException e) {
+                System.out.println(e.getMessage());
+                resp = null;
+            } finally {
+                if (!req.isNotify()) {
+                    resps.add(resp);
                 }
             }
         }
+
         try {
             //una richiesta singola può essere inviata come un arraylist di dimensione 1
             serverJsonRpc.reply(resps);
@@ -75,54 +85,50 @@ public class Server {
         }
     }
 
-
-    private static final String CREA_TOKEN = "creatoken";
-    private static final String VERIFICA_TOKEN = "verificatoken";
-    private static final String CREA_AUTORIZAZIONE = "creaautorizzazione";
-    private static final String VERIFICA_ESISTENZA_AUTORIZZAZIONE = "verificaesistenzaautorizzazione";
-    private static final String CREA_RISORSA = "crearisorsa";
-    private static final String MODIFICA_RISORSA = "modificarisorsa";
-    private static final String CANCELLA_RISORSA = "cancellarisorsa";
-
-
-    private Member selectMethod(String method, StructuredMember params)  throws InvalidParameterException, NoSuchMethodException {
+    private Member selectMethod(Methods method, StructuredMember params)  throws InvalidParameterException {
         ArrayList<Member> p = new ArrayList<>();
         try {
             if (params!=null) //i parametri sono opzionali
                 p = params.getList();
         } catch (ClassCastException c) {
-            throw new InvalidParameterException();
+            throw new InvalidParameterException("Not a parameters list");
         }
 
-        switch (method) {
-            case CREA_TOKEN:
-                try {
-                    //verificare non ci siano troppi parametri
-                    return Member.toMember(tokenManager.creaToken(p.get(0).getString(), p.get(1).getInt()));
-                } catch (JSONRPCException e) {
-                    throw new InvalidParameterException();
-                }
-                /*altre eccezioni interne al server non gestite dalla select method ma dal receive che crea risposta con errore corrispondente*/
+        if (p.size() != method.getParamsNum()) {throw new InvalidParameterException("Wrong parameters number");}
 
-            case VERIFICA_TOKEN:
-                try {
-                    return Member.toMember(tokenManager.verificaToken(p.get(0).getString(), p.get(1).getInt()));
-                } catch (JSONRPCException e) {
-                    throw new InvalidParameterException();
-                }
-
-            case CREA_AUTORIZAZIONE:
-
-                break;
-            case VERIFICA_ESISTENZA_AUTORIZZAZIONE:
-                break;
-            case CREA_RISORSA:
-                break;
-            case MODIFICA_RISORSA:
-                break;
-            case CANCELLA_RISORSA:
-                break;
-            default: throw new NoSuchMethodException("Method " + method + " not found");
+        try {
+            switch (method) {
+                case CREA_TOKEN:
+                    String token = tokenManager.creaToken(p.get(0).getString(), p.get(1).getInt());
+                    return new Member(token);
+                case VERIFICA_TOKEN:
+                    //far restituire un tipo data
+                    long time = tokenManager.verificaToken(p.get(0).getString(), p.get(1).getInt());
+                    return new Member(time);
+                case CREA_AUTORIZAZIONE:
+                    DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                    Date date = dateFormat.parse(p.get(2).getString());
+                    String key = authManager.creaAutorizzazione(p.get(0).getString(), p.get(1).getInt(), date);
+                    return new Member(key);
+                case VERIFICA_ESISTENZA_AUTORIZZAZIONE:
+                    boolean existance = authManager.verificaEsistenzaAutorizzazione(p.get(0).getString());
+                    return new Member(existance);
+                case CREA_RISORSA:
+                    //resourceManager.creaRisorsa(...);
+                    break;
+                case MODIFICA_RISORSA:
+                    //resourceManager.modificaRisorsa(...);
+                    break;
+                case CANCELLA_RISORSA:
+                    //resourceManager.cancellaRisorsa(...);
+                    break;
+            }
+        } catch (ClassCastException e) {
+            throw new InvalidParameterException("Wrong parameter type: " + e.getMessage());
+        } catch (ParseException e) {
+            throw new InvalidParameterException("Invalid date format: " + e.getMessage());
+        } catch (JSONRPCException e) {
+            System.out.println(e.getMessage());
         }
         return null;
     }
