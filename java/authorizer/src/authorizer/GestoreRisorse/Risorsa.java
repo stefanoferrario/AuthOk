67de9b5f6ca487e65eb44189f5adea25fcaf5d3e
paolo <paolo.isella@mail.polimi.it>
2017-12-12 22:30:53 +0000

    Upload New File

---
+++ java/authorizer/src/authorizer/GestoreRisorse/Risorsa.java
@@ -0,0 +1,17 @@
+package sistemacentrale.gestorerisorse;
+
+public abstract class Risorsa {
+	
+	private int livello = 0;
+	private int id = 0;
+
+	public Risorsa(int _id, int _livello){id = _id;	livello = _livello;}
+	
+	public int getLivello() {return livello;}
+
+	public int getId() {return id;}
+
+	public void setLivello(int _livello) {livello = _livello;}
+
+	public void setId(int _id) {id = _id;}
+}
