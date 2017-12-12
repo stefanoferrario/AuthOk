ce8f0d375b00123b9d8451ea7872ba4e8d9b0c05
paolo <paolo.isella@mail.polimi.it>
2017-12-12 22:30:07 +0000

    Upload New File

---
+++ java/authorizer/src/authorizer/GestoreRisorse/FactoryFibonacci.java
@@ -0,0 +1,14 @@
+package sistemacentrale.gestorerisorse;
+
+class FactoryFibonacci implements FactoryRisorsa {
+
+	@Override
+	public Risorsa creaRisorsa(int _id, int _livello) {
+		return new RisorsaFibonacci(_id, _livello);
+	}
+
+	@Override
+	public Risorsa creaRisorsa(int _id) {
+		return new RisorsaFibonacci(_id, 1);
+	}
+}
